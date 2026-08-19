37e779e25bac669e96a90b351b56e5bb30f90a48
ryanuo <1577791638@qq.com>
2026-08-19 15:32:44 +0800

    feat(docs): 添加代码高亮语言支持
    添加了 QML、C++ 和 C 语言的代码高亮配置，
    以增强文档中相关代码示例的可读性。

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,3 +1,12 @@
 export default defineNuxtConfig({
   extends: ['docus'],
+  content: {
+    build: {
+      markdown: {
+        highlight: {
+          langs: ['qml', 'cpp', 'c']
+        }
+      }
+    }
+  }
 })
